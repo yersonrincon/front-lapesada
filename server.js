e0d5b12b50cc1a0b024f18cb843c139826d9d580
fabianHer <fabian.hernandez1908@gmail.com>
2021-11-19 13:29:43 -0500

    primer commit

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require('path');
 const app = express();
 app.use(express.static(`${__dirname}/docs/tifonFront`));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(`${__dirname}/docs/tifonFront/index.html`));
+    res.sendFile(path.join(`${__dirname}/docs/index.html`));
 });
 app.listen(process.env.PORT || 8080);
