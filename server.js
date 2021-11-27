1b8c183bb742ab95d36c55405d232fa25db3da59
fabianHer <fabian.hernandez1908@gmail.com>
2021-11-26 19:12:15 -0500

    tercer commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(`${__dirname}/docs/tifonFront`));
-app.get('*', function(req, res) {
+app.get('/*', function(req, res) {
     res.sendFile(path.join(`${__dirname}/docs/index.html`));
 });
 app.listen(process.env.PORT || 8080);
