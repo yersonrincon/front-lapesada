54a5eb40b60bceb3ace3fc5c1444c0e80316f523
fabianHer <fabian.hernandez1908@gmail.com>
2021-11-27 12:13:40 -0500

    cuarto commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(`${__dirname}/docs/tifonFront`));
+app.use(express.static(`${__dirname}/docs`));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(`${__dirname}/docs/index.html`));
 });
