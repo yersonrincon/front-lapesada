c0471b1eec038b4e71de547c44ea8e1abe6dbc1e
yerson <yersonhernandez202@gmail.com>
2022-09-30 18:18:55 -0500

    cors agregados

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   
- //apiUrl: 'https://lapesada-bk.herokuapp.com' 
- apiUrl: 'http://localhost:8445'
+ apiUrl: 'https://lapesada-bk.herokuapp.com' 
+ //apiUrl: 'http://localhost:8445'
 
 
 };
