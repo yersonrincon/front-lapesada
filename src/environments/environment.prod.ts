09cfe739d534fbebc91397e205b698e1e223381a
yersonrincon <yersonhernandez202@gmail.com>
2023-05-20 00:31:26 -0500

    tercer commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   
    //apiUrl: 'https://lapesada-bk.herokuapp.com' 
-   apiUrl: 'http://localhost:8445'
+  // apiUrl: 'http://localhost:8445'
+ apiUrl:"https://lapesada-back.onrender.com"
 
 
 };
